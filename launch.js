a67990eac75d76cc679ad60ddc018cac942e0666
ISh- <imwaves@gmail.com>
2015-04-01 18:59:04 +0300

    ready

--- launch.js
+++
@@ -1,9 +0,0 @@
-chrome.app.runtime.onLaunched.addListener(function() {
-  chrome.app.window.create('index.html', {
-	id: "mainwin",
-    innerBounds: {
-      width: 640,
-      height: 480
-    }
-  });
-});
